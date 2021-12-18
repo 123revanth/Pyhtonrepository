53541bf7035c4bde5ba654559814a5e5473474c5
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2021-12-18 14:29:12 +0530

    Create simeOPS.py

---
+++ simeOPS.py
@@ -0,0 +1,23 @@
+def sum(one, two):
+    return one + two
+
+def difference(one, two):
+    return one - two
+
+def product(one, two):
+    return one * two
+
+def quotient(one, two):
+    return one / two
+
+def reminder(one, two):
+    return one % two
+
+one = int(input("Enter the first number"))
+two = int(input("Enter the second number"))
+
+print(sum(one, two))
+print(difference(one, two))
+print(product(one, two))
+print(quotient(one, two))
+print(reminder(one, two))
