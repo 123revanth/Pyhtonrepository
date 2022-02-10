025e2e786af856708f645b4ec6e70768fbafd14a
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-02-10 10:28:17 +0530

    Update ElectionEligibilty.py

--- ElectionEligibilty.py
+++ ElectionEligibilty.py
@@ -5,6 +5,6 @@ if(age >= 18):
 else:
     print("you are not eligible to vote")
     
-print("Don't worry u will can vote after few years")
+print("Don't worry u can vote after few years")
 print("if you have already voted then good luck for your elected person to win")
 
