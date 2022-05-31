e3a1c2707c602022ce88e8475fad1e6d6b26c672
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-05-31 14:59:05 +0530

    Create reverseWords.py

---
+++ reverseWords.py
@@ -0,0 +1,17 @@
+from tkinter import N
+
+
+def convert(string):
+    listed = list(string.split(" "))
+    return listed
+
+string = raw_input("Enter a string:")
+
+arr = list()
+
+arr = convert(string)
+
+arr.reverse()
+
+for i in range(0, len(arr)):
+    print arr[i],
