65b5fb728726db9395da5f0df463a31c912f8e87
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-06-18 12:19:47 +0530

    Create HTTPrequestthroughsocket.py

---
+++ HTTPrequestthroughsocket.py
@@ -0,0 +1,30 @@
+import socket
+from unicodedata import name
+from xml.dom.minidom import CharacterData
+
+
+HOST = 'www.youtube.com'  # Server hostname or IP address
+PORT = 80                # The standard port for HTTP is 80, for HTTPS it is 443
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (HOST, PORT)
+client_socket.connect(server_address)
+
+request_header = b'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
+client_socket.sendall(request_header)
+
+response = ''
+while True:
+    recv = client_socket.recv(1024)
+    if not recv:
+        break
+    response += str(recv)
+
+print(response)
+client_socket.close()
+
+
+
+
+    
+ 
