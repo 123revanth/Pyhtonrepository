b6dde118fa9cee5cf081018b94b8a6e74518d826
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-07-02 10:50:26 +0530

    Delete FetchingTweets.py

--- FetchingTweets.py
+++
@@ -1,8 +0,0 @@
-import tweepy, os 
-
-def fetch_tweets_from_user(user_name):
-    auth = tweepy.OAuthHandler(os.environ['TWITTER_KEY'], os.environ['TWITTER_SECRET'])
-    auth.set_access_token(os.environ['TWITTER_TOKEN'], os.environ['TWITTER_TOKEN_SECRET'])
-    api = tweepy.API(auth)
-    tweets = api.user_timeline(screen_name=user, count=200, include_rts=False)
-    return tweets
