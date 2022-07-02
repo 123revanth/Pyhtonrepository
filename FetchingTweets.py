11d6dc8e1435a614b3d71482b8dbcdfc82beff5e
Revanth jallipalli <81552258+123revanth@users.noreply.github.com>
2022-07-02 10:48:58 +0530

    Create FetchingTweets.py

---
+++ FetchingTweets.py
@@ -0,0 +1,10 @@
+import tweepy, os 
+
+def fetch_tweets_from_user(user_name):
+    # authentification
+    auth = tweepy.OAuthHandler(os.environ['TWITTER_KEY'], os.environ['TWITTER_SECRET'])
+    auth.set_access_token(os.environ['TWITTER_TOKEN'], os.environ['TWITTER_TOKEN_SECRET'])
+    api = tweepy.API(auth)
+    # fetch tweets
+    tweets = api.user_timeline(screen_name=user, count=200, include_rts=False)
+    return tweets
